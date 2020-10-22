bec25768f2004812fa2c3887948523a9c97255d9
Sadia Rashid <srashi25@my.centennialcollege.ca>
2020-10-22 01:03:28 -0400

    models and structure folders added

---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group4_Lab3.Models
+{
+    [DynamoDBTable("Movie")]
+    public class Movie
+    {
+        [DynamoDBHashKey]
+        public string MovieId{get; set;}
+        public string Genre { get; set; }
+        public DateTime Date { get; set; }
+        public List<Review> Reviews { get; set; }
+        public string Rating { get; set; }
+
+    }
+}
---
+++ Models/UserMovies.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group4_Lab3.Models
+{
+    [DynamoDBTable("UserMovies")]
+    public class UserMovies
+    {
+        [DynamoDBHashKey]
+        public string UserMoviesId { get; set; }
+        public string UserEmail { get; set; }
+        public List<Movie> ListUserMovies { get; set; }
+    }
+}
