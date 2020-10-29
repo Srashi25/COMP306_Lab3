5b1b8df1483027de77ee198410d7b24b883ebdc3
Charles Thomson <charlie.thomson86@gmail.com>
2020-10-29 00:17:54 -0400

    Upload and Review pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,5 +19,7 @@ namespace Charles_Sadia_Lab3.Controllers
         public IActionResult Index() => View();
 
         public IActionResult Upload() => View();
+
+        public IActionResult Review() => View();
     }
 }
